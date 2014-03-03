965c47b980cb1b9dd08b89edc5f90864abe264cf
Kyle Conrad <kyle@kyleconrad.com>
2014-03-03 17:32:23 -0500

    Initial case study and page layout designed

--- config.rb
+++ config.rb
@@ -6,7 +6,6 @@ sass_dir = "sass"
 css_dir = "css"
 images_dir = "img"
 javascripts_dir = "js"
-fonts_dir = "fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
--- prod/js/footer.js
+++ prod/js/footer.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
+	var windowWidth = $(window).width(),
+		windowHeight = $(window).height();
+	var isMobile = navigator.userAgent.match(/mobile/i);
 
+	// WIDOW CONTROL
+	if (windowWidth > 640) {
+		$('h1, h2, h3, li, p, figcaption').each(function() {
+		    $(this).html($(this).html().replace(/\s((?=(([^\s<>]|<[^>]*>)+))\2)\s*$/,'&nbsp;$1'));
+		});
+	}
+
+	// LAZY LOADING
+	$('.lazy').show().lazyload({
+		effect: 'fadeIn',
+		skip_invisible: false,
+		placeholder: 'img/trans.png',
+		threshold: (windowHeight / 2)
+	});
 });
